7d56f46db066a27a3a1e2ee231b570fccbad80da
pooyan <thepooyan@gmail.com>
2025-09-11 16:18:03 +0330

    restructure

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
-import { PurgeCSS } from "purgecss"
-import { compileSassFiles, findUnusedSelectors } from "./util"
+import { compileSassFiles, findUnusedSelectors, mapSassImports, readFilesFromPatterns, traceSelectorToOrigin } from "./util"
 
 export interface props {
     content: string[]
     scss: string[]
 }
 export const purgeSASS = async (props:props) => {
+    const contentFiles = readFilesFromPatterns(props.content)
+    const compiledSass = compileSassFiles(props.scss)
 
-    let purgeResult = await findUnusedSelectors(props)
+
+    let purgeResult = await findUnusedSelectors({rawContent: contentFiles, rawCss: compiledSass})
+    mapSassImports()
+    traceSelectorToOrigin()
     console.log(purgeResult)
 }
 
--- src/util.ts
+++ src/util.ts
@@ -1,17 +1,33 @@
 import {globSync} from "glob"
+import fs from "fs"
 import * as sass from "sass"
-import type { props } from "."
 import { PurgeCSS } from "purgecss"
+import path from "path"
 
 export const compileSassFiles = (files: string[]) => {
     let fileNames = globSync(files)
     return fileNames.map(f => ({raw: sass.compile(f).css, name: f}) )
 }
+export const readFilesFromPattern = (pattern:string) => {
+    const files = globSync(pattern)
+    return files.map(f => ({raw: fs.readFileSync(f, "utf-8"), name: f, extension: path.extname(f)}))
+}
+export const readFilesFromPatterns = (patterns: string[]) => patterns.map(readFilesFromPattern).flat()
 
-export const findUnusedSelectors = async ({content, scss}:props) => {
+interface rawCSS {name: string, raw: string}
+interface rawFile {extension: string, raw: string}
+interface rawProps {
+    rawContent: rawFile[]
+    rawCss: rawCSS[]
+}
+export const findUnusedSelectors = async ({rawContent, rawCss}:rawProps) => {
     return await new PurgeCSS().purge({
-        content: content,
-        css: compileSassFiles(scss) ,
+        content: rawContent,
+        css: rawCss,
         rejected: true
     })
 }
+
+export const mapSassImports = () => {}
+
+export const traceSelectorToOrigin = () => {}
