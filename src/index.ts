f3aeb8e64e89bf98ba8bbcb20a16bc2d7957cb89
pooyan <thepooyan@gmail.com>
2025-09-12 11:41:58 +0330

    logs

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,22 @@ export interface props {
 }
 export const purgeSASS = async (props:props) => {
     const contentFiles = readFilesFromPatterns(props.content)
+
+    console.log(`Found ${contentFiles.length} content files.`)
+    console.log(contentFiles.map(c => c.name))
+
     const compiledSass = compileSassFiles(props.scss)
 
+    console.log(`Found ${compiledSass.length} Sass files.`)
+    console.log(compiledSass.map(c => c.name))
 
     let purgeResult = await findUnusedSelectors({rawContent: contentFiles, rawCss: compiledSass})
+    console.log(`Found ${purgeResult.reduce((p,c) => c.rejected?.length || 0 + p, 0)} unused selectors across ${purgeResult.length} files.`)
+    console.log(purgeResult.map(p => ({file: p.file, count: p.rejected?.length || 0}) ))
+
     let dependencyGraph = await mapSassImports(props.scss)
     analyzeAndPurge(purgeResult, dependencyGraph)
+    console.log("Done!")
 }
 
 
--- src/mainFunctions.ts
+++ src/mainFunctions.ts
@@ -2,6 +2,7 @@ import { PurgeCSS, type ResultPurge } from "purgecss"
 import { purgeSassSelectorsFromFile, type finalResult, type rawProps } from "./util"
 import { analyzeSassDependencies, type DependencyGraph } from "./graph"
 import { globSync } from "glob"
+import { unsafe } from "bun"
 
 export const findUnusedSelectors = async ({rawContent, rawCss}:rawProps) => {
     return await new PurgeCSS().purge({
@@ -23,12 +24,17 @@ export const traceSelectorToOrigin = (purgeResult: ResultPurge[], dependencyGrap
         if (!unused.file) continue
         let deps = dependencyGraph.get(unused.file || "")
         if (!deps) { //File has no dependcies, all unused selectors are in root
+            console.log(`Purging ${unused.rejected.length} selectors from ${unused.file}`)
+            console.log({file: unused.file, selectors: unused.rejected})
+
             purgeSassSelectorsFromFile(unused.file, unused.rejected)
             continue
         }
         //file has dependencies, read them and assign 
         for (const dep of deps) {
             purgeSassSelectorsFromFile(dep, unused.rejected)
+            console.log(`Purging ${unused.rejected.length} selectors from ${dep}`)
+            console.log({file: dep, selectors: unused.rejected})
         }
         purgeSassSelectorsFromFile(unused.file, unused.rejected)
     }
